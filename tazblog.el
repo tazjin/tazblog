cec8b8873fab4ccd0a5f016b5691179427aa2357
Vincent Ambo <tazjin@gmail.com>
2017-11-13 23:00:09 +0100

    feat(tazblog): Use elblog's new extra route feature for legacy links
    Instead of adding the old articles into the elblog-articles variable
    twice (with old and new names), use an elblog custom route to add a
    handler that properly redirects users to the new post location.

--- tazblog.el
+++ tazblog.el
@@ -1,13 +1,16 @@
-;; tazjin's blog
+;; tazjin's blog.
+;; -*- lexical-binding: t; -*-
 
 (require 'elblog)
 (require 'ht)
+
+;; Blog entry configuration
 
 (defun map-entry (entry)
   "Automatically map an entry to its corresponding org-mode file."
   `(,entry . ,(concat entry ".org")))
 
-(defvar tazblog-entries
+(setq elblog-articles
   (let ((entries '("reversing-watchguard-vpn"
                    "make-object-t-again"
                    "letsencrypt-controller"
@@ -20,29 +23,31 @@
                    "nsa-zettabytes")))
     (ht<-alist (mapcar #'map-entry entries))))
 
-;; To ensure that legacy links still work, keep this mapping. Forever.
-;; TODO: Maybe it is actually possible to do this mapping as a URL rewrite
-;; instead.
-(defvar tazblog-legacy-links
-  (ht
-   ("1375310627" "nsa-zettabytes.org")
-   ("1423995834" "sick-in-sweden.org")
-   ("1448288135" "masterless-puppet.org")
-   ("1448375823" "end-of-dynlangs.org")
-   ("1450354078" "the-smu-problem.org")
-   ("1455623225" "servant-presentation.org")
-   ("1467448422" "kubernetes-presentation.org")
-   ("1474235830" "letsencrypt-controller.org")
-   ("1476807384" "make-object-t-again.org")
-   ("1486830338" "reversing-watchguard-vpn.org")))
-
-(setq elblog-articles
-      (ht-merge tazblog-entries tazblog-legacy-links))
 (setq elblog-article-directory default-directory)
 
-;; By default elblog will include all entries in the elblog-entries list in the
-;; index.
-;; Due to the legacy-alias setup above this would be a bit silly, so this
-;; returns tazblog-entries directly.
-(setq elblog-index-filter-func (lambda (_) tazblog-entries))
+;; Legacy link setup
 
+;; To ensure that legacy links still work, keep this mapping. Forever.
+(defvar tazblog-legacy-ids
+  (ht
+   ("1375310627" "nsa-zettabytes")
+   ("1423995834" "sick-in-sweden")
+   ("1448288135" "masterless-puppet")
+   ("1448375823" "end-of-dynlangs")
+   ("1450354078" "the-smu-problem")
+   ("1455623225" "servant-presentation")
+   ("1467448422" "kubernetes-presentation")
+   ("1474235830" "letsencrypt-controller")
+   ("1476807384" "make-object-t-again")
+   ("1486830338" "reversing-watchguard-vpn")))
+
+(defun legacy-link-handler (httpcon)
+  "Redirect legacy links (/en/{article-id}) to new format (/{article-name})."
+  (letrec ((legacy-id (elnode-http-mapping httpcon 1))
+           (article-name (ht-get tazblog-legacy-ids legacy-id)))
+    (message "legacy handler called for %s" article-name)
+    (elnode-send-redirect
+     httpcon (concat "/" (or article-name "not-found")))))
+
+(setq elblog-additional-routes
+      '(("^.*//en/\\(.*\\)$" . legacy-link-handler)))
